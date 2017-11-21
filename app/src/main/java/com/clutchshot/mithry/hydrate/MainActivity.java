94b1a51bfb6c463e8327a1f03a99d260354ddcb0
mithrycode <32175501+mithrycode@users.noreply.github.com>
2017-11-21 02:54:42 +0200

    1st notification try changed icon

--- app/src/main/java/com/clutchshot/mithry/hydrate/MainActivity.java
+++ app/src/main/java/com/clutchshot/mithry/hydrate/MainActivity.java
@@ -1,13 +1,44 @@
 package com.clutchshot.mithry.hydrate;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import java.security.KeyStore;
 
 public class MainActivity extends AppCompatActivity {
+
+    NotificationCompat.Builder notification;
+    private static final int uniqueID = 45612;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notification = new NotificationCompat.Builder(this, "45612");
+        notification.setAutoCancel(true);
     }
+
+    public void clickNotifClicked(View view){
+        //Builds the notif
+        notification.setSmallIcon(R.drawable.ic_launcher2);
+        notification.setTicker("Drink some water!");
+        notification.setWhen(System.currentTimeMillis());
+        notification.setContentTitle("Here is the title");
+        notification.setContentText("You need to drink that glass of water, bro!");
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.setContentIntent(pendingIntent);
+
+        //Build notification and issuing it
+        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        nm.notify(uniqueID, notification.build());
+    }
+
 }
